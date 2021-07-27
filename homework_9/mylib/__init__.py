236d81cd0174c322a42682dcbf116e46d262d5f3
Nikita Limarchuk <nikita.limarchuk@gmail.com>
2021-07-27 23:53:13 +0300

    HW9: add described issues in comments

--- homework_9/mylib/__init__.py
+++ homework_9/mylib/__init__.py
@@ -2,3 +2,4 @@ from homework_9.mylib.simple_arithmetic import sum_number
 from homework_9.mylib.simple_arithmetic import delta_number
 from homework_9.mylib.hard_arithmetic import sqr_number
 from homework_9.mylib.hard_arithmetic import res_arithmetic
+# well not absolute imports on my side so this code will never work on m;y side
