cffa59242e68f62ccef1918645af929609c7f6fc
Arpine Agazarian <arpine.agazarian@gmail.com>
2021-08-01 21:43:58 +0200

    homework_10

---
+++ homework_10/task_1.py
@@ -0,0 +1,138 @@
+class Company:
+    """A class used to represent the Company object
+
+    Attributes
+    ----------
+    name: str
+        the name of the Company. Required
+    foundation_year: int
+        the foundation year of Company. Required
+    email: str
+        email of Company
+    employee_amount: int
+        amount of employees in Company
+    budget: float
+        budget of Company
+    income: float
+        income of Company
+
+    Methods
+    -------
+    create_new_company(cls, name: str, foundation_year: int)
+        This method creates new instance of Company with required fields: name and foundation_year
+    get_company_info(self)
+         Return all company's information
+    get_profitability(self)
+         Return the profitability in percents. Income divided by budget and multiplied by 100
+    """
+
+    def __init__(
+            self,
+            name: str,
+            foundation_year: int,
+            email: str = None,
+            employee_amount: int = None,
+            budget: float = None,
+            income: float = None
+    ):
+        self.__name = name
+        self.__foundation_year = foundation_year
+        self.__email = email
+        self.__employee_amount = employee_amount
+        self.__budget = budget
+        self.__income = income
+
+    @property
+    def name(self):
+        return self.__name
+
+    @name.setter
+    def name(self, name) -> None:
+        self.__name = name
+
+    @property
+    def foundation_year(self) -> int:
+        return self.__foundation_year
+
+    @foundation_year.setter
+    def foundation_year(self, foundation_year) -> None:
+        self.__foundation_year = foundation_year
+
+    @property
+    def email(self):
+        return self.__email
+
+    @email.setter
+    def email(self, email) -> None:
+        self.__email = email
+
+    @property
+    def employee_amount(self):
+        return self.__employee_amount
+
+    @employee_amount.setter
+    def employee_amount(self, employee_amount) -> None:
+        self.__employee_amount = employee_amount
+
+    @property
+    def budget(self):
+        return self.__budget
+
+    @budget.setter
+    def budget(self, budget) -> None:
+        self.__budget = budget
+
+    @property
+    def income(self):
+        return self.__income
+
+    @income.setter
+    def income(self, income) -> None:
+        self.__income = income
+
+    @classmethod
+    def create_new_company(cls, name: str, foundation_year: int):
+        """This method creates new instance of Company with required fields: name and foundation_year"""
+        return cls(name, foundation_year)
+
+    def get_profitability(self) -> float:
+        """This method returns result of profitability calculation"""
+        if type(self.__income) == float and\
+                self.__income > 0 and\
+                type(self.__budget) == float and\
+                self.__budget > 0:
+            profitability = self.__income / self.__budget * 100
+        else:
+            raise Exception("Impossible to calculate profitability")
+        return profitability
+
+    def get_company_info(self):
+        """This method returns a string with all company information"""
+        return (f"company name: {self.__name}\n"
+                f"foundation year: {self.__foundation_year}\n"
+                f"email: {self.__email}\n"
+                f"employee_amount: {self.__employee_amount}\n"
+                f"budget: {self.__budget}\n"
+                f"income: {self.__income}\n"
+                f"profitability: {self.get_profitability()}\n")
+
+
+if __name__ == '__main__':
+    global_logic: Company = Company.create_new_company("Global Logic", 2003)
+    toshiba: Company = Company.create_new_company("Toshiba", 1987)
+
+    global_logic.email = "info@globallogic.com"
+    global_logic.employee_amount = 4500
+    global_logic.budget = 4500000.00
+    global_logic.income = 98000.87
+
+    toshiba.email = "info@toshiba.com"
+    toshiba.employee_amount = 12348
+    toshiba.budget = 12000233.98
+    toshiba.income = 8700234.32
+
+    print("Global Logic:")
+    print(global_logic.get_company_info())
+
+    print("\nToshiba:")
+    print(toshiba.get_company_info())
---
+++ homework_10/task_2.py
@@ -0,0 +1,146 @@
+from datetime import date
+
+
+class Employee:
+    """A class used to represent the Employee object
+
+        Attributes
+        ----------
+        first_name: str
+            the first name of the Employee. Required
+        last_name: str
+            the last name of the Employee. Required
+        birth_date: date
+            date of birthday of Employee. Required
+        start_date: date
+            date when Employee starts work. Required
+        is_fired: bool
+            information about Employee:is hired or no. Required
+        position: str
+            Employee's position. Optional
+        salary: float
+            Employee's salary Optional
+
+        Methods
+        -------
+        def new_employee(cls,
+                     first_name: str,
+                     last_name: str,
+                     birth_date: date,
+                     start_date: date,
+                     is_fired: bool)
+             This method creates new instance of Employee with required fields
+        get_full_name(self)
+             This method returns full Employee's name
+        get_full_info(self)
+             This method returns full info about Employee
+        """
+
+    def __init__(
+            self,
+            first_name: str,
+            last_name: str,
+            birth_date: date,
+            start_date: date,
+            is_fired: bool,
+            position: str = None,
+            salary: float = None
+    ):
+        self.__first_name = first_name
+        self.__last_name = last_name
+        self.__birth_date = birth_date
+        self.__start_date = start_date
+        self.__is_fired = is_fired
+        self.__position = position
+        self.__salary = salary
+
+    @property
+    def first_name(self):
+        return self.__first_name
+
+    @first_name.setter
+    def first_name(self, first_name) -> None:
+        self.__first_name = first_name
+
+    @property
+    def last_name(self):
+        return self.__last_name
+
+    @last_name.setter
+    def last_name(self, last_name) -> None:
+        self.__last_name = last_name
+
+    @property
+    def birth_date(self):
+        return self.__birth_date
+
+    @birth_date.setter
+    def birth_date(self, birth_date) -> None:
+        self.__birth_date = birth_date
+
+    @property
+    def start_date(self):
+        return self.__start_date
+
+    @start_date.setter
+    def start_date(self, start_date) -> None:
+        self.__start_date = start_date
+
+    @property
+    def is_fired(self):
+        return self.__is_fired
+
+    @is_fired.setter
+    def is_fired(self, is_fired) -> None:
+        self.__is_fired = is_fired
+
+    @property
+    def position(self):
+        return self.__position
+
+    @position.setter
+    def position(self, position) -> None:
+        self.__position = position
+
+    @property
+    def salary(self):
+        return self.__salary
+
+    @salary.setter
+    def salary(self, salary) -> None:
+        self.__salary = salary
+
+    @classmethod
+    def new_employee(cls,
+                     first_name: str,
+                     last_name: str,
+                     birth_date: date,
+                     start_date: date,
+                     is_fired: bool):
+        """This method creates new instance of Employee with required fields"""
+        return cls(first_name,
+                   last_name,
+                   birth_date,
+                   start_date,
+                   is_fired)
+
+    def get_full_name(self):
+        """This method returns full Employee's name """
+        return f"{self.__first_name} {self.__last_name}"
+
+    def get_full_info(self):
+        """This method returns full info about Employee"""
+        return (f"full name: {self.get_full_name()}\n"
+                f"birth date: {self.__birth_date}\n"
+                f"start_date: {self.__start_date}\n"
+                f"is_fired: {self.__is_fired}\n"
+                f"position: {self.__position}\n"
+                f"salary: {self.__salary}\n")
+
+
+if __name__ == '__main__':
+    employee_1: Employee = Employee.new_employee('Arpine', 'Agazarian', date(1990, 12, 27), date(2020, 1, 28), False)
+
+    employee_1.position = "QA"
+    employee_1.salary = 5000.550
+    print(employee_1.get_full_info())
