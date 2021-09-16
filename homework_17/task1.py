30b8bef844f4c21ca5f2df6bcd94e6ec15e21e89
Nikita Limarchuk <nikita.limarchuk@gmail.com>
2021-09-16 21:05:34 +0300

    HW17: almost perfect

--- homework_17/task1.py
+++ homework_17/task1.py
@@ -22,16 +22,18 @@ class TestEmail:
         self.driver.get("https://www.oneskyapp.com")
 
         email_input = self.driver.find_element_by_xpath(self.email_input_xpath)
-        get_started_for_free_button = self.driver.find_element_by_xpath(self.submit_button_xpath)
+        get_started_for_free_button = self.driver.find_element_by_xpath(
+            self.submit_button_xpath
+        )
 
         action = ActionChains(self.driver)
-        action \
-            .move_to_element(email_input) \
-            .perform()
+        action.move_to_element(email_input).perform()
 
         email_input.send_keys("uyfhigfgfuttflhfu")
         get_started_for_free_button.click()
 
-        actual_error_message = self.driver.find_element_by_xpath(self.error_message_xpath)
+        actual_error_message = self.driver.find_element_by_xpath(
+            self.error_message_xpath
+        )
         assert actual_error_message.is_displayed() is True
         assert actual_error_message.text == self.expected_error_message
